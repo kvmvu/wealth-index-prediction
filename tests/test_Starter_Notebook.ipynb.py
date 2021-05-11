67beac9390bfb48cb9c3774b3419f7b5ae76a471
Eric Kamau <kamauericm@gmail.com>
2021-05-12 02:13:10 +0300

    evaluate model by country

---
+++ tests/test_Starter_Notebook.ipynb.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_split_by_country(self):
+        self.fail()
+
+    def test_model_performance(self):
+        self.fail()
+
+    def test_evaluate_model_per_country(self):
+        self.fail()
